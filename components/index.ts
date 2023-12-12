b2fa027c80e9cfb82ebccb0db612d2cbde75c9aa
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-12 23:57:00 +0500

    Exported different components

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export { default as Create } from './crud/create/Create';
+export { default as Read } from './crud/read/Read';
+export { default as Update } from './crud/update/Update';
+export { default as Delete } from './crud/delete/Delete';
+export { default as Input } from './Input';
+export { default as Button } from './Button';
+export { default as QueryProvider } from './QueryProvider';
